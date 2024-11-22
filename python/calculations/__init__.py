f4303ee75867518544b057e5913f976cb62c3090
troegeraaron <troeger.aaron@gmail.com>
2024-11-21 23:01:16 -0500

    Added python folder for fun

---
+++ python/calculations/__init__.py
@@ -0,0 +1,17 @@
+# create my own calculator!
+
+class Calculator():
+    def add(self, x, y):
+        return x + y
+    
+    def subtract(self, x, y):
+        return x - y
+    
+    def multiply(self, x, y):
+        return x * y
+    
+    def divide(self, x, y):
+        if y == 0:
+            return "Divide by 0 error"
+        return x / y
+    
